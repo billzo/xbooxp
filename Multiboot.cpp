b4345e86b8ed51166484542a2ffe74d0bb3b67c9
zero.vishnu <zero.vishnu@gmail.com>
2013-02-02 16:22:29 +0000

    Added secondary files. Added license and readme. Added license notifications in code.

--- Multiboot.cpp
+++ Multiboot.cpp
@@ -1,3 +1,21 @@
+/*
+	This file contains the main routine for the xbooxp program. It makes
+	use of io.dll, which is free, and available at
+	http://www.geekhideout.com/iodll.shtml
+
+    xbooxp is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    xbooxp is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with xbooxp.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #include "stdio.h"
 #include "conio.h"
 #include "io.h"
--- io.h
+++ io.h
@@ -1,3 +1,20 @@
+/*
+	This file is part of xbooxp. It makes use of io.dll, which is free, and
+	available at http://www.geekhideout.com/iodll.shtml
+
+    xbooxp is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    xbooxp is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with xbooxp.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #include "windows.h"
 
 typedef void (WINAPI *PORTOUT) (short int Port, char Data);
